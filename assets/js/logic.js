1d936e5e0549dff812b9ca67dde43899cdfa0845
Anthony Potts <anthonypotts@Anthonys-MacBook-Pro-2.local>
2021-05-12 14:59:33 -0500

    added highscores.html and functionality in js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -21,4 +21,123 @@ function startQuiz() {
     timerId = setInterval(clockTick, 1000);
 
     timerEl.textContent = time;
+
+    getQuestion();
 }
+
+function getQuestion() {
+    // get question from questions.js
+    var currentQuestion = questions[currentQuestionIndex];
+
+    // update title
+    var titleEl = document.getElementById("q-title");
+    titleEl.textContent = currentQuestion.title;
+
+    // clear old question choices
+    optionsEl.innerHTML = "";
+
+    // loop choices
+    currentQuestion.options.forEach(function(choice, i) {
+        //creates button for option
+        var optionNode = document.createElement("button");
+        optionNode.setAttribute("class", "option");
+        optionNode.setAttribute("value", option);
+        optionNode.textContent = i + 1 + "." + option;
+        
+        // attach click event listener to option
+        optionNode.onclick = questionClick;
+
+        // display to html page
+        optionsEl.appendChild(optionNode);
+        
+    });
+}
+
+function questionClick() {
+    // check for wrong answer
+    if (this.value !== questions[currentQuestionIndex].answer) {
+        // penalty
+        time -= 15;
+
+        if (time < 0) {
+            time = 0;
+        }
+
+        // display time on page
+        timerEl.textContent = time;
+        
+        feedbackEl.textContent = "Wrong";
+    } else {
+        feedbackEl.textContent = "Correct!";
+    }
+
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+        feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+
+    // next question!
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex === questions.length) {
+        quizEnd();
+    } else {
+        getQuestion();
+    }
+}
+
+function quizEnd() {
+    //stop timer
+    clearInterval(timerId);
+
+    var endScreenEl = document.getElementById("end-screen");
+    endScreenEl.removeAttribute("class");
+
+    var finalScoreEl = document.getElementById("final-score");
+    finalScoreEl.textContent = time;
+
+    questionsEl.setAttribute("class", "hide");
+}
+
+function clockTick() {
+    time--;
+    timerEl.textContent = time;
+
+    if (time <= 0) {
+        quizEnd();
+    }
+}
+
+function saveHighscore() {
+    var initials = initialsEl.value.trim();
+
+    if (initials !== "") {
+        var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+        var newScore = {
+            score: time,
+            initials: initials
+        };
+
+        highscores.push(newScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+        window.location.href = "highscores.html";
+    }
+}
+
+function checkForEnter(event) {
+    // "13" represents the enter key
+    if (event.key === "Enter") {
+      saveHighscore();
+    }
+  }
+  
+  // user clicks button to submit initials
+  submitBtn.onclick = saveHighscore;
+  
+  // user clicks button to start quiz
+  startBtn.onclick = startQuiz;
+  
+  initialsEl.onkeyup = checkForEnter;
+  
